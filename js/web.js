32f352326c0d42dc0022b9f39145aabb1a68c9c3
DELL <tuongmynga@gmail.com>
2022-02-17 10:29:19 +0700

    3c

--- js/web.js
+++ js/web.js
@@ -1,14 +1,14 @@
-var btn = document.querySelector('.sign');
-var hien = document.querySelector('.auth-form');
-var daux = document.querySelector('.daux');
-var boddy = document.querySelector('#pape-wraper');
-var buton = document.querySelector('.btn_btn');
-btn.addEventListener('click',function(){
-    hien.classList.add('hien');
-});
-daux.addEventListener('click',function(){
-    hien.classList.remove('hien');
-});
-buton.addEventListener('click',function(){
-	boddy.classList.remove('hien');
-});
+// var btn = document.querySelector('.sign');
+// var hien = document.querySelector('.auth-form');
+// var daux = document.querySelector('.daux');
+// var boddy = document.querySelector('#pape-wraper');
+// var buton = document.querySelector('.btn_btn');
+// btn.addEventListener('click',function(){
+//     hien.classList.add('hien');
+// });
+// daux.addEventListener('click',function(){
+//     hien.classList.remove('hien');
+// });
+// buton.addEventListener('click',function(){
+// 	boddy.classList.remove('hien');
+// });
